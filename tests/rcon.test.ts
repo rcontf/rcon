121bf38eabd93dcea9658785d263f528d539389e
c43721 <rlahman.game@gmail.com>
2025-09-13 17:29:05 -0500

    feat: add some starter tests

---
+++ tests/rcon.test.ts
@@ -0,0 +1,52 @@
+import assert from "node:assert";
+import { Rcon } from "../src/rcon.ts";
+
+Deno.test("Rcon can authenticate", async () => {
+  using rcon = new Rcon({ host: "127.0.0.1", port: 27015 });
+
+  const didAuthenticate = await rcon.authenticate("password");
+
+  assert.equal(didAuthenticate, true);
+});
+
+Deno.test("Rcon will not authenticate on a bad password", async () => {
+  using rcon = new Rcon({ host: "127.0.0.1", port: 27015 });
+
+  const didAuthenticate = await rcon.authenticate("badpassword");
+
+  assert.equal(didAuthenticate, false);
+});
+
+Deno.test("Rcon returns the result of the command as a string", async () => {
+  using rcon = new Rcon({ host: "127.0.0.1", port: 27015 });
+
+  const didAuthenticate = await rcon.authenticate("password");
+
+  assert.equal(didAuthenticate, true);
+
+  const result = await rcon.execute("echo hello");
+
+  assert.equal(result, "hello");
+});
+
+Deno.test({
+  name: "Rcon successfully returns multi packet responses",
+  ignore: true,
+  fn: async () => {
+    using rcon = new Rcon({ host: "127.0.0.1", port: 27015 });
+
+    const didAuthenticate = await rcon.authenticate("password");
+
+    assert.equal(didAuthenticate, true);
+
+    const result = await rcon.execute("cvarlist");
+
+    await Deno.writeTextFile("test.txt", result, { create: true });
+
+    const expectedResult = await Deno.readTextFile(
+      "tests/fixtures/multi-packet-response.txt",
+    );
+
+    assert.strictEqual(result, expectedResult);
+  },
+});
